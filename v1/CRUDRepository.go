9150b8c76916647f09979e8638e5ac5b576d9af0
Kosha <igornem1@gmail.com>
2025-03-29 19:19:44 +0200

    next

--- v1/CRUDRepository.go
+++ v1/CRUDRepository.go
@@ -11,7 +11,7 @@ const (
 	RETURNING_ID = "RETURNING id"
 )
 
-type PostgreSQLCRUDRepository[T any] struct {
+type CRUDRepository[T any] struct {
 	db              DbClient
 	insertBuilder   sq.InsertBuilder
 	selectBuilder   sq.SelectBuilder
@@ -20,20 +20,20 @@ type PostgreSQLCRUDRepository[T any] struct {
 	entityConverter func(row pgx.Row) T
 }
 
-func NewPostgreSQLCRUDRepository[T any](
+func NewCRUDRepository[T any](
 	db DbClient,
 	insertBuilder sq.InsertBuilder,
 	selectBuilder sq.SelectBuilder,
 	updateBuilder sq.UpdateBuilder,
 	deleteBuilder sq.DeleteBuilder,
-	entityConverter func(row pgx.Row) T) PostgreSQLCRUDRepository[T] {
-	return PostgreSQLCRUDRepository[T]{
+	entityConverter func(row pgx.Row) T) CRUDRepository[T] {
+	return CRUDRepository[T]{
 		db:            db,
 		insertBuilder: insertBuilder, selectBuilder: selectBuilder, updateBuilder: updateBuilder, deleteBuilder: deleteBuilder,
 		entityConverter: entityConverter}
 }
 
-func (repo PostgreSQLCRUDRepository[T]) Create(ctx context.Context, values ...interface{}) int64 {
+func (repo CRUDRepository[T]) Create(ctx context.Context, values ...interface{}) int64 {
 	builder := repo.insertBuilder.Suffix(RETURNING_ID).Values(values...)
 	var id int64
 	err := repo.db.API().QueryRowContextInsert(ctx, builder).Scan(&id)
@@ -43,13 +43,13 @@ func (repo PostgreSQLCRUDRepository[T]) Create(ctx context.Context, values ...in
 	return id
 }
 
-func (repo PostgreSQLCRUDRepository[T]) GetById(ctx context.Context, id int64) T {
+func (repo CRUDRepository[T]) GetById(ctx context.Context, id int64) T {
 	builder := repo.selectBuilder.Where(sq.Eq{idColumn: id})
 	row := repo.db.API().QueryRowContextSelect(ctx, builder)
 	return repo.entityConverter(row)
 }
 
-func (repo PostgreSQLCRUDRepository[T]) ConvertToObjects(rows pgx.Rows) []T {
+func (repo CRUDRepository[T]) ConvertToObjects(rows pgx.Rows) []T {
 	var objs []T
 	for rows.Next() {
 		obj := repo.entityConverter(rows)
@@ -61,20 +61,20 @@ func (repo PostgreSQLCRUDRepository[T]) ConvertToObjects(rows pgx.Rows) []T {
 	return objs
 }
 
-func (repo *PostgreSQLCRUDRepository[T]) GetAll(ctx context.Context) []T {
+func (repo *CRUDRepository[T]) GetAll(ctx context.Context) []T {
 	rows := repo.db.API().QueryContextSelect(ctx, repo.selectBuilder, nil)
 	objs := repo.ConvertToObjects(rows)
 	return objs
 }
 
-func (repo *PostgreSQLCRUDRepository[T]) GetBy(ctx context.Context, where sq.Sqlizer) []T {
+func (repo *CRUDRepository[T]) GetBy(ctx context.Context, where sq.Sqlizer) []T {
 	builder := repo.selectBuilder.Where(where)
 	rows := repo.db.API().QueryContextSelect(ctx, builder, nil)
 	objs := repo.ConvertToObjects(rows)
 	return objs
 }
 
-func (repo *PostgreSQLCRUDRepository[T]) Delete(ctx context.Context, id int64) int64 {
+func (repo *CRUDRepository[T]) Delete(ctx context.Context, id int64) int64 {
 	builder := repo.deleteBuilder.Where(sq.Eq{idColumn: id})
 	return repo.db.API().ExecDelete(ctx, builder)
 }
@@ -86,17 +86,17 @@ func updateInternal(ctx context.Context, api DbApi, updateBuilder sq.UpdateBuild
 	return api.ExecUpdate(ctx, updateBuilder)
 }
 
-func (repo *PostgreSQLCRUDRepository[T]) Update(ctx context.Context, fields map[string]interface{}, id int64) int64 {
+func (repo *CRUDRepository[T]) Update(ctx context.Context, fields map[string]interface{}, id int64) int64 {
 	builder := repo.updateBuilder.Where(sq.Eq{idColumn: id})
 	return updateInternal(ctx, repo.db.API(), builder, fields)
 }
 
-func (repo *PostgreSQLCRUDRepository[T]) UpdateCollection(ctx context.Context, fields map[string]interface{}, where sq.Sqlizer) int64 {
+func (repo *CRUDRepository[T]) UpdateCollection(ctx context.Context, fields map[string]interface{}, where sq.Sqlizer) int64 {
 	builder := repo.updateBuilder.Where(where)
 	return updateInternal(ctx, repo.db.API(), builder, fields)
 }
 
-func (repo *PostgreSQLCRUDRepository[T]) UpdateReturning(ctx context.Context, builder sq.UpdateBuilder, entityConverter func(row pgx.Row) T) T {
+func (repo *CRUDRepository[T]) UpdateReturning(ctx context.Context, builder sq.UpdateBuilder, entityConverter func(row pgx.Row) T) T {
 	row := repo.db.API().UpdateReturning(ctx, builder)
 	return entityConverter(row)
 }
